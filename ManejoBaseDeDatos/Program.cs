088269edb27911e51c9f9af514b702d78a85dbd9
rusokverse <rusokverse@MACBOOKPRO>
2023-02-22 21:08:09 -0400

    Agregar archivos de proyecto.

---
+++ ManejoBaseDeDatos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManejoBaseDeDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            conectandonos();
+        }
+        public static void conectandonos() {
+            string cadenaConexion = @"Server=192.168.1.253;DataBase=Instituto X;User=sa;";// lo normal: .\SQLDEVELOPERCQ
+        }
+    }
+}
